6d626f75a600952a0e0d7a258d7f567b4aacb6c3
xyrix <xyrixorg@gmail.com>
2013-12-11 23:10:59 +0000

    changed snetris block from black to magenta; updated to have moving blocks - need to refactor lots of behavior into classes...

--- main.js
+++ main.js
@@ -82,7 +82,10 @@ var BLOCKS = [
 
 var fallingBlock = {};
 var nextTetrisMove = 0;
+var nextTetrisRotate = 0;
+var nextTetrisHori = 0;
 var tetrisMoveInterval = 1000;
+var tetrisControlInterval = 150;
 
 
 function preload() {
@@ -100,26 +103,30 @@ function create() {
 		spriteGroup.createMultiple(200, 'snetris', BLOCKS[i]['sprite']);
 		BLOCKS[i]['sprite'] = spriteGroup;
 	}
-	grid = new Array(10);
-	for (var i = 0; i < 10; ++i)
-		grid[i] = new Array(20);
+	grid = new Array(12);
+	for (var i = 0; i < 12; ++i) {
+		grid[i] = new Array(22);
+		for (var j = 0; j < 22; ++j)
+			grid[i][j] = i == 0 || j == 0 || i == 11 || j == 21;
+	}
 
 	// Draw grid
 	var graphics = game.add.graphics(0, 0);
 	graphics.beginFill(0x00ff00);
 	graphics.lineStyle(1, 0x00ff00, 1);
 	for (var i = 0; i < 11; ++i) {
-		graphics.moveTo(i * 16, 0);
-		graphics.lineTo(i * 16, 320);
+		graphics.moveTo(16 + i * 16, 16);
+		graphics.lineTo(16 + i * 16, 336);
 	}
 	for (var i = 0; i < 21; ++i) {
-		graphics.moveTo(0, i * 16);
-		graphics.lineTo(160, i * 16);
+		graphics.moveTo(16, 16 + i * 16);
+		graphics.lineTo(176, 16 + i * 16);
 	}
 	graphics.endFill();
 
 	// Start game
 	tetrisStartDrop();
+	nextTetrisMove = game.time.now + tetrisMoveInterval;
 }
 
 function computeRotations(block) {
@@ -149,18 +156,35 @@ function update() {
 		nextTetrisMove += tetrisMoveInterval;
 		tetrisDoMove();
 	}
+	var rot = controls['tetrisRotLeft'].isDown ||
+		controls['tetrisRotRight'].isDown ||
+		controls['tetrisUp'].isDown;
+	if (rot && game.time.now > nextTetrisRotate) {
+		tetrisDoRot();
+	}
+	var leftRight = controls['tetrisLeft'].isDown ||
+		controls['tetrisRight'].isDown;
+	if (leftRight && game.time.now > nextTetrisHori) {
+		tetrisDoLeftRight();
+	}
 }
 
 function tetrisStartDrop() {
+	// TODO: check for collision
 	var type = Math.floor(Math.random() * BLOCKS.length);
 	var rotation = 0;
 	var shape = BLOCKS[type]['shape'][rotation];
-	var x = Math.floor((10 - shape.length) / 2);
-	var y = 0;
+	var x = Math.ceil((10 - shape.length) / 2) + 1;
+	var y = 1;
 	fallingBlock['type'] = type;
 	fallingBlock['rotation'] = 0;
 	fallingBlock['x'] = x;
 	fallingBlock['y'] = y;
+	addBlockToGrid(type, x, y, shape);
+	addShape(x, y, shape);
+}
+
+function addBlockToGrid(type, x, y, shape) {
 	var sprites = [];
 	for (var i = 0; i < shape.length; ++i)
 		for (var j = 0; j < shape.length; ++j)
@@ -173,7 +197,81 @@ function tetrisStartDrop() {
 }
 
 function tetrisDoMove() {
-	sprites = fallingBlock['sprites'];
-	for (var i = 0; i < sprites.length; ++i)
-		sprites[i].y += 16;
+	var x = fallingBlock['x'];
+	var y = fallingBlock['y'];
+	var shape = BLOCKS[fallingBlock['type']]['shape'][fallingBlock['rotation']];
+	removeShape(x, y, shape);
+	if (checkCollision(x, y + 1, shape)) {
+		addShape(x, y, shape);
+		tetrisStartDrop();
+	} else {
+		fallingBlock['y'] = y + 1;
+		var sprites = fallingBlock['sprites'];
+		for (var i = 0; i < sprites.length; ++i)
+			sprites[i].y += 16;
+		addShape(x, y + 1, shape);
+	}
+}
+
+function checkCollision(x, y, shape) {
+	for (var i = 0; i < shape.length; ++i)
+		for (var j = 0; j < shape.length; ++j)
+			if (shape[i][j] && grid[x + i][y + j])
+				return true;
+	return false;
+}
+
+function addShape(x, y, shape) {
+	for (var i = 0; i < shape.length; ++i)
+		for (var j = 0; j < shape.length; ++j)
+			if (shape[i][j])
+				grid[x + i][y + j] = true;
+}
+
+function removeShape(x, y, shape) {
+	for (var i = 0; i < shape.length; ++i)
+		for (var j = 0; j < shape.length; ++j)
+			if (shape[i][j])
+				grid[x + i][y + j] = false;
+}
+
+function tetrisDoRot() {
+	var left = controls['tetrisRotRight'].isDown;
+	var direction = left ? 1 : 3;
+	var x = fallingBlock['x'];
+	var y = fallingBlock['y'];
+	var type = fallingBlock['type'];
+	var rot = fallingBlock['rotation'];
+	var newRot = (rot + direction) % 4;
+	removeShape(x, y, BLOCKS[type]['shape'][rot]);
+	if (checkCollision(x, y, BLOCKS[type]['shape'][newRot]))
+		addShape(x, y, BLOCKS[type]['shape'][rot]);
+	else {
+		fallingBlock['rotation'] = newRot;
+		addShape(x, y, BLOCKS[type]['shape'][newRot]);
+		var sprites = fallingBlock['sprites'];
+		for (var i = 0; i < sprites.length; ++i)
+			sprites[i].kill();
+		addBlockToGrid(type, x, y, BLOCKS[type]['shape'][newRot]);
+		nextTetrisRotate = game.time.now + tetrisControlInterval;
+	}
+}
+
+function tetrisDoLeftRight() {
+	left = controls['tetrisLeft'].isDown;
+	var dx = left ? -1 : 1;
+	var x = fallingBlock['x'];
+	var y = fallingBlock['y'];
+	var shape = BLOCKS[fallingBlock['type']]['shape'][fallingBlock['rotation']];
+	removeShape(x, y, shape);
+	if (checkCollision(x + dx, y, shape))
+		addShape(x, y, shape);
+	else {
+		fallingBlock['x'] += dx;
+		addShape(x + dx, y, shape);
+		var sprites = fallingBlock['sprites'];
+		for (var i = 0; i < sprites.length; ++i)
+			sprites[i].x += 16 * dx;
+		nextTetrisHori = game.time.now + tetrisControlInterval;
+	}
 }
